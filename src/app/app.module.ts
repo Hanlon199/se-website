29a0ce7cc367014891055feb9ee98b4e5cf22033
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2018-11-26 20:36:21 -0500

    first commit
    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule} from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClassesComponent } from './classes/classes.component';
+import { AccoladesComponent } from './accolades/accolades.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { StudentsComponent } from './students/students.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'classes', component: ClassesComponent},
+  {path: 'accolades', component: AccoladesComponent},
+  {path: 'faculty', component: FacultyComponent},
+  {path: 'students', component: StudentsComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClassesComponent,
+    AccoladesComponent,
+    FacultyComponent,
+    StudentsComponent,
+    NavigationComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+	currentPath:any;
+  	constructor() { 
+  		let temp = window.location.pathname.split('/') 
+  		this.currentPath = temp[1];
+  	}
+
+  ngOnInit() {
+  }
+
+}
